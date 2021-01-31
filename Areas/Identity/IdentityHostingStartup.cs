af7d31ae09a30a908cc382b9a6c68dd8aae1e1dc
Bezifabr <bezifabr@gmail.com>
2021-01-31 16:03:40 +0100

    Initial commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LoginPage.Areas.Identity.Data;
+using LoginPage.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LoginPage.Areas.Identity.IdentityHostingStartup))]
+namespace LoginPage.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+
+                var builder = new SqlConnectionStringBuilder(context.Configuration.GetConnectionString("LoginPageDBContextConnection"));
+                services.AddDbContext<LoginPageDBContext>(options => options.UseSqlServer(builder.ConnectionString));
+
+                services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<LoginPageDBContext>();
+            });
+        }
+    }
+}
