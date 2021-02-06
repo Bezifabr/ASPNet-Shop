458c31e4db08ece23ab0766e8897f8ecd9dca37f
Bezifabr <bezifabr@gmail.com>
2021-02-06 14:25:15 +0100

    Implement ShopItems management in AdminPanel

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shop.Models;
+using Shop.Views.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,22 +11,60 @@ using System.Threading.Tasks;
 
 namespace Shop.Controllers
 {
+    [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
+        ShopItemsContext _shopItemsContext;
 
+        public AdminController(ShopItemsContext shopItemsContext)
+        {
+            _shopItemsContext = shopItemsContext;
+        }
 
         [Route("AdminPanel")]
-        [Authorize(Roles = "admin")]
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: AdminController/Details/5
-        public ActionResult Details(int id)
+        [Route("AdminPanel/ShopItemsManager")]
+        public ActionResult ShopItems()
+        {
+            ViewBag.ShopItems = _shopItemsContext.ShopItems;
+            return View();
+        }
+
+        [Route("AdminPanel/ShopItemsManager/Add")]
+        public ActionResult ShopItemCreator()
         {
             return View();
         }
+
+        ShopItemDetailsModel shopItemDetailsModel = new ShopItemDetailsModel();
+
+        [Route("AdminPanel/Details/{id:int}")]
+        public ActionResult ShopItemDetails(int id)
+        {
+            foreach (var item in _shopItemsContext.ShopItems)
+            {
+                if (item.id == id)
+                {
+                    ViewBag.Item = item;
+                    break;
+                }
+            }
+
+
+
+            return View(shopItemDetailsModel);
+        }
+
+        [Route("AdminPanel/ShopItemsCategoriesManager")]
+        public ActionResult ShopItemsCategories()
+        {
+            return View();
+        }
+
 
         // GET: AdminController/Create
         public ActionResult Create()
@@ -46,10 +87,31 @@ namespace Shop.Controllers
             }
         }
 
-        // GET: AdminController/Edit/5
-        public ActionResult Edit(int id)
+        [HttpPost]
+        public void ChangeShopItem(ShopItem shopItem)
         {
-            return View();
+            var item = _shopItemsContext.ShopItems.FirstOrDefault(s => s.id == shopItem.id);
+            item.name = shopItem.name;
+            item.description = shopItem.description;
+            item.pictureUrl = shopItem.pictureUrl;
+            item.price = shopItem.price;
+
+            _shopItemsContext.SaveChanges();
+        }
+
+        [HttpPost]
+        public void AddShopItem(ShopItem shopItem)
+        {
+            _shopItemsContext.ShopItems.Add(shopItem);
+            _shopItemsContext.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void RemoveShopItem(int id)
+        {
+            var shopItem = _shopItemsContext.ShopItems.SingleOrDefault(s => s.id == id);
+            if (shopItem != null) _shopItemsContext.Remove(shopItem);
+            _shopItemsContext.SaveChanges();
         }
 
         // POST: AdminController/Edit/5
