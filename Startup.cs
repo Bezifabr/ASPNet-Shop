355d79018a3a621599ec33d44a1992788a328361
Bezifabr <bezifabr@gmail.com>
2021-02-06 10:13:27 +0100

    Prepare AdminController and add AdminPanel to navbar

--- Startup.cs
+++ Startup.cs
@@ -74,9 +74,9 @@ namespace LoginPage
 
             app.UseEndpoints(endpoints =>
             {
-            endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
